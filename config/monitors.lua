0c23e447e369bda959dedd4794f3e84224c6d5bf
efollman <evan.follman@gmail.com>
2026-06-24 11:57:54 -0500

    vrr 3 didnt work

--- config/monitors.lua
+++ config/monitors.lua
@@ -13,7 +13,7 @@ hl.config({
 
 hl.config({
 	misc = {
-		vrr = 3, -- video or game content 3, set to 1 or 2 (fullscreen only) if this causes issues/ doesnt work
+		vrr = 1, -- video or game content 3, set to 1 or 2 (fullscreen only) if this causes issues/ doesnt work
 	},
 	quirks = {
 		prefer_hdr = 2, --gamescope only
